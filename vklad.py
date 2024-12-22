bc7ec20f18239db250a37559a61202ec111c906b
123.py <bronnikov.maxon@gmail.com>
2024-12-22 17:54:54 +0700

    Update vklad.py

--- vklad.py
+++ vklad.py
@@ -4,17 +4,9 @@ Token = '7955185006:AAGUu8tpE0MX9RrkVhqycnaD9Iap-Dm1ru0'
 bot = TeleBot(Token)
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    k = types.InlineKeyboardMarkup()
-    j = types.InlineKeyboardButton('да', callback_data='0')
-    l = types.InlineKeyboardButton('нет', callback_data='1')
-    k.add(j)
-    k.add(l)
-
-
-
-
-    bot.send_message(message.chat.id,'Вы пользовались нашей программой ранее',reply_markup=k)
+     
     con = sqlite3.connect('vklad.db')
+    
     sql = con.execute ("""
         CREATE TABLE IF NOT EXISTS vk(
         
@@ -29,6 +21,22 @@ def handle_start(message):
 
 
     """)
+    
+    k = types.InlineKeyboardMarkup()
+    
+    j = types.InlineKeyboardButton('да', callback_data='0')
+
+    l = types.InlineKeyboardButton('нет', callback_data='1')
+    gh = types.InlineKeyboardButton('Показать мой вклад', callback_data='2')
+    k.add(j)
+    k.add(l)
+    k.add(gh)
+    bot.send_message(message.chat.id,'Вы пользовались нашей программой ранее',reply_markup=k)
+
+
+
+
+
 def textic(message):
     
     con = sqlite3.connect('vklad.db')
@@ -89,13 +97,48 @@ def textis(message):
 
 @bot.callback_query_handler(func=lambda callback:True)
 def handle_callback(callback):
+    con = sqlite3.connect('vklad.db')
     if callback.data == '1':
 
         bot.send_message(callback.message.chat.id,'Введите сумму вклада')
         bot.register_next_step_handler(callback.message,textic)
-    else:
+    elif callback.data == '0':
         bot.send_message(callback.message.chat.id,'Процент вклада(только число)')
         bot.register_next_step_handler(callback.message,textis)
+    else:
+        
+        bot.send_message(callback.message.chat.id,'Вот')
+        sql = con.execute(f'''
+        SELECT sum FROM vk
+        WHERE chat = {callback.message.chat.id}   
+        ''').fetchall()
+        sql = sql[0][-1]
+        sql2 = con.execute(f'''
+        SELECT proc FROM vk
+        WHERE chat = {callback.message.chat.id}   
+        ''').fetchall()
+        sql2 = sql2[0][-1]
+        sql3 = con.execute(f'''
+        SELECT plus FROM vk
+        WHERE chat = {callback.message.chat.id}   
+        ''').fetchall()
+        sql3 = sql3[0][-1]
+        sql4 = con.execute(f'''
+        SELECT hola FROM vk
+        WHERE chat = {callback.message.chat.id}   
+        ''').fetchall()
+        sql4 = sql4[0][-1]
+
+        if sql == []:
+            bot.send_message(callback.message.chat.id,'Вы впервые пользуетесь программой')    
+            
+        else:
+
+            bot.send_message(callback.message.chat.id,f'Текущая сумма вклада: {sql}\nТекущий процент вклада: {sql2}\nТекущий плюс к вкладу: {sql3}\nИтог: {sql4}')
+        
+
+        
+
 
         
 
